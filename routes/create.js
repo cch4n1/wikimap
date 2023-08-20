bf046bc36c47366458e99e5d9ae7b131f19c0f7d
Bo Yang Yu <boyangyu93@gmail.com>
2023-08-20 04:29:26 -0400

    add query to add new map, only with valid name, and update home page to display query info for all maps in db

--- routes/create.js
+++ routes/create.js
@@ -7,16 +7,33 @@
 
 const express = require('express');
 const router  = express.Router();
+const createMapQuery = require('../db/queries/create-map');
 
 router.get('/', (req, res) => {
   res.render('create');
 });
 
-router.post('/', (req, res) => {
-  if (1===1) {
-    res.redirect('/maps/edit/1')
-  }
-});
+// router.post('/', (req, res) => {
+//   if (1===1) {
+//     res.redirect('/profile')
+//   }
+// });
 
+router.post('/', (req, res) => {
+  // console.log(req.body.mapTitle);
+  const mapParams = req.body.mapTitle;
+  createMapQuery.createMap(mapParams)
+    .then(map => {
+      // console.log(map);
+      console.log('---------->>> this is a test of the route');
+      res.redirect('/profile');
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+  // res.redirect('/profile');
+});
 
 module.exports = router;
