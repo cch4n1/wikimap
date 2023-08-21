ff55675fb69dd343e1a7ce6f9a41c870d7255a44
Jubril Bello <jbello@uoguelph.ca>
2023-08-20 23:00:57 -0400

    Separate all seeds into their own files

---
+++ db/seeds/users_seeds.sql
@@ -0,0 +1,7 @@
+-- user seed --
+INSERT INTO users (email, password, username) VALUES ('alice@example.com', 'alicepassword', 'Alice');
+INSERT INTO users (email, password, username) VALUES ('kira@example.com', 'kirapassword', 'Kira');
+INSERT INTO users (email, password, username) VALUES ('thirduser@example.com', 'thirduserpassword', 'ThirdUser');
+
+
+
