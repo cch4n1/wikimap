306b26947688debd2ee4d524d4bbac8a156bbc83
Charlie C <zbyabqrc@pm.me>
2023-08-21 05:27:49 +0000

    updated view map page to load sql query data

---
+++ db/queries/get-points-for-map-view.js
@@ -0,0 +1,22 @@
+const db = require('../connection');
+
+/**
+ * get Points function
+ * takes in the map_id for a specific map
+ * and returns a list of all the names and descriptions 
+ * of all the points on the map
+ * @param {integer} mapId
+ * @returns 
+ */
+const getPoints = (mapId) => {
+  return db.query(`
+  SELECT title as Point_of_Interest, description 
+  FROM points
+  WHERE map_id = ${mapId};
+  `)
+    .then(data => {
+      return data.rows;
+    });
+};
+
+module.exports = { getPoints };
