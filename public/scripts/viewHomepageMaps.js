23d9ce30a7ba45917e0ca4442e2e9a644b91882a
Jubril Bello <jbello@uoguelph.ca>
2023-08-20 23:19:26 -0400

    Add Ajax to script to get the list of maps

---
+++ public/scripts/viewHomepageMaps.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-undef */
+// <!-- Script to fetch map data and populate the table -->
+$(document).ready(function() {
+  $.ajax({
+    method: "GET",
+    url: "/api/viewHomepageMaps",
+  }).done(function(response) {
+    const maps = response.maps;
+
+    for (const map of maps) {
+      const tableRow = `
+        <tr>
+          <td>${map.title}</td>
+          <td>${map.username}</td>
+          <td>
+            <form method="POST" action="#">
+              <button type="submit" class="btn btn-outline-danger">Show Map</button>
+            </form>
+          </td>
+        </tr>
+      `;
+
+      $("#mapData").append(tableRow);
+    }
+  });
+});
