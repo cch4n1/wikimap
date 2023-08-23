fbb3147cda9b00199f308adff6c589926c37111a
Charlie C <zbyabqrc@pm.me>
2023-08-23 06:29:46 +0000

    map pins now display saved db image on in-map pop-up view, added new select query for map data and routed to viewMap page, updated viewMap page to display Map Name from db as Headings

--- db/seeds/maps_seeds.sql
+++ db/seeds/maps_seeds.sql
@@ -1,4 +1,4 @@
 -- map seed --
 INSERT INTO maps (user_id, title, latitude, longtitude) VALUES (1, 'Favorite Park', 34.0522, -118.2437);
 INSERT INTO maps (user_id, title, latitude, longtitude) VALUES (2, 'Secret Beach', -33.8688, 151.2093);
-INSERT INTO maps (user_id, title, latitude, longtitude) VALUES (3, 'City View', 40.7128, -74.0060);
+INSERT INTO maps (user_id, title, latitude, longtitude) VALUES (3, 'Day Trip in London', 51.505, -0.09);
