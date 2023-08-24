2398d5ad2b4a0e61d9bca735e0acbf5cb222259c
Jubril Bello <jbello@uoguelph.ca>
2023-08-24 14:20:15 -0400

    Fetch the profile by username

--- routes/viewProfileUsernameById-api.js
+++ routes/viewProfileUsernameById-api.js
@@ -1,21 +1,50 @@
+// const express = require("express");
+// const router = express.Router();
+// const userQueries = require("../db/queries/getUsernameById");
+
+// router.get("/", (req, res) => {
+//   let userId = req.params.userId;
+//   //   change this
+//   userId = 3;
+//   //   change this
+//   userQueries
+//     .getUsernameById(userId)
+//     .then((username) => {
+//       console.log(username);
+//       res.json({ username });
+//     })
+//     .catch((err) => {
+//       res.status(500).json({ error: err.message });
+//     });
+// });
+
+// module.exports = router;
+
 const express = require("express");
 const router = express.Router();
 const userQueries = require("../db/queries/getUsernameById");
 
 // router.get("/:userId", (req, res) => {
-router.get("/", (req, res) => {
-  let userId = req.params.userId;
-  //   change this
-  userId = 3;
-  //   change this
+
+// http://localhost:8080/profile/1
+
+router.get("/:userId", (req, res) => {
+  const userId = req.params.userId;
+
   userQueries
     .getUsernameById(userId)
     .then((username) => {
-      console.log(username);
-      res.json({ username });
+      if (username) {
+        // Render the profile view with the username
+        res.render("profile", { user: { id: userId, username: username } });
+      } else {
+        // Handle case when username is not found for the given userId
+        res.status(404).send("Username not found for the given User ID");
+      }
     })
     .catch((err) => {
-      res.status(500).json({ error: err.message });
+      console.error(`Error: ${err}`);
+      res.status(500).send("Server Error");
     });
 });
 
