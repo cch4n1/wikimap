60c2a505231358528af8868f4a29d7ef105a3298
Jubril Bello <jbello@uoguelph.ca>
2023-08-24 05:11:01 -0400

    Render different homepages depending on the user

--- routes/home.js
+++ routes/home.js
@@ -5,20 +5,56 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const express = require('express');
-const router  = express.Router();
-const displayMapQuery = require('../db/queries/display-all-maps');
+const express = require("express");
+const router = express.Router();
+const displayMapQuery = require("../db/queries/display-all-maps");
+const userQueries = require("../db/queries/getUsernameById"); // Import the needed function
 
-router.get('/', (req, res) => {
 
-  displayMapQuery.displayAllMaps()
+// For non-logged in users
+router.get("/", (req, res) => {
+  displayMapQuery
+    .displayAllMaps() // Query to display all maps for homepage
     .then((maps) => {
       const templateVars = {
-        maps
-      }
-      console.log(maps);
-      res.render('index', templateVars)
+        maps: maps,
+        user: null, // no user logged in
+      };
+      res.render("index", templateVars); // render the homepage
     })
+    .catch((err) => {
+      res.status(500).send(err.message);
+    });
+});
+
+// For logged in users --> http://localhost:8080/home/1
+router.get("/:id", (req, res) => {
+  const userId = req.params.id;
+
+  // Fetch maps and user data in parallel
+  Promise.all([
+    displayMapQuery.displayAllMaps(), // display all maps on the homepage
+    userQueries.getUsernameById(userId), // get username using :id from browser
+  ])
+    .then(([maps, username]) => {
+      console.log("Maps Data:", maps);
+      console.log("User Data:", username);
+
+      if (username) {
+        const templateVars = {
+          maps: maps,
+          user: { id: userId, username: username }, // Using the directly returned username
+        };
+        console.log("Template Variables:", templateVars);
+        res.render("index", templateVars); // pass the username to home view using template vars
+      } else {
+        res.status(404).send("User not found");
+      }
+    })
+    .catch((err) => {
+      console.error("Error occurred:", err); // Logging the error with console.error for emphasis
+      res.status(500).send(err.message);
+    });
 });
 
 module.exports = router;
