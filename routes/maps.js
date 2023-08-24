961f70045d28b22666e94574946f05f382f51d26
Jubril Bello <jbello@uoguelph.ca>
2023-08-24 14:17:10 -0400

    Add routes to see user favorites

--- routes/maps.js
+++ routes/maps.js
@@ -5,22 +5,39 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const mapQueries = require("../db/queries/toggleFavorite");
 
 //view map
-router.get('/1', (req, res) => {
-  res.render('viewMap');
+router.get("/1", (req, res) => {
+  res.render("viewMap");
 });
 
 //edit map
-router.get('/edit/1', (req, res) => {
-  res.render('editMap');
-})
+router.get("/edit/1", (req, res) => {
+  res.render("editMap");
+});
 
 //delete map
-router.post('/1/delete', (req, res) => {
-  res.redirect('profile');
+router.post("/1/delete", (req, res) => {
+  res.redirect("profile");
+});
+
+router.post("/favourite/:mapId", (req, res) => {
+  console.log(true);
+  const mapId = req.params.mapId;
+  const shouldBeFavorited = req.body.favourited === "true";
+
+  mapQueries
+    .toggleFavorite(mapId, shouldBeFavorited)
+    .then(() => {
+      res.json({ success: true });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.json({ success: false });
+    });
 });
 
 module.exports = router;
