362e819311b17905766f5e1a2f0a2a1301933d0d
Jubril Bello <jbello@uoguelph.ca>
2023-08-24 14:15:11 -0400

    Render the faves page

--- routes/faves.js
+++ routes/faves.js
@@ -5,11 +5,37 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const userQueries = require("../db/queries/getUsernameById");
+const mapQueries = require("../db/queries/getFavoriteMapsByUserId"); // Adjust path as necessary
 
 router.get('/', (req, res) => {
   res.render('faves');
-}); 
+});
+
+router.get("/:userId", (req, res) => {
+  const userId = req.params.userId;
+
+  Promise.all([
+    userQueries.getUsernameById(userId),
+    mapQueries.getFavoriteMapsByUserId(userId),  // Use the new function
+  ])
+    .then(([username, maps]) => {
+      if (username) {
+        res.render("faves", {  // Assuming you have a separate template for favorites
+          user: { id: userId, username: username },
+          userMaps: maps,
+        });
+      } else {
+        res.status(404).send("User not found for the given User ID");
+      }
+    })
+    .catch((err) => {
+      console.error("Error occurred:", err);
+      res.status(500).send("Server Error");
+    });
+});
+
 
 module.exports = router;
