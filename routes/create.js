431de6fda5e40d4edd468640493af1ee9b7754b0
Bo Yang Yu <boyangyu93@gmail.com>
2023-08-25 01:19:19 -0400

    fix routes for maps

--- routes/create.js
+++ routes/create.js
@@ -7,36 +7,68 @@
 
 const express = require('express');
 const router  = express.Router();
+const userQueries = require("../db/queries/getUsernameById");
+const mapQueries = require("../db/queries/getMapsByUserId"); // Add this line
 const createMapQuery = require('../db/queries/create-map');
 
-router.get('/', (req, res) => {
-  res.render('create');
-});
+router.get('/:userId', (req, res) => {
+  const userId = req.params.userId;
+  console.log('username is' + userId);
+  // console.log(req.body);
+  Promise.all([
+    userQueries.getUsernameById(userId),
+    mapQueries.getMapsByUserId(userId),
+  ])
+    .then((results) => {
+      // console.log("All Promises Resolved:", results);
 
-// router.post('/', (req, res) => {
-//   if (1===1) {
-//     res.redirect('/profile')
-//   }
-// });
+      const username = results[0];
+      const maps = results[1];
+      // console.log("Username:", username);
+      // console.log("Maps:", maps);
 
-router.post('/', (req, res) => {
-  // console.log(req.body.mapTitle);
-  console.log(req.body);
-  console.log(req.query);
-  console.log(req.params);
-  console.log('testing reqs');
-  const mapParams = req.body.mapTitle;
-  createMapQuery.createMap(mapParams)
-    .then(map => {
-      console.log(map);
-      console.log('---------->>> this is a test of the route');
-      res.redirect('/profile');
+      if (username) {
+        // Render the profile view with the username, maps, and ID
+        res.render("create", {
+          user: { id: userId, username: username },
+          userMaps: maps,
+        });
+      } else {
+        // Handle the case when the username isn't found for the given userId
+        res.status(404).send("User not found for the given User ID");
+      }
     })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: err.message });
+    .catch((err) => {
+      console.error("Error in Promise.all:", err);
+      res.status(500).send("Server Error");
     });
 });
+
+router.post('/:userId', (req, res) => {
+  const userId = req.params.userId;
+  console.log(req.body);
+
+  const mapParams = {
+    id: userId,
+    title: req.body.mapTitle
+  };
+
+  createMapQuery.createMap(mapParams)
+    .then(maps => {
+      const createdMap = maps[0];
+      console.log('Created Map:', createdMap);
+      console.log('User ID:', createdMap.user_id);
+      console.log('---------->>> this is a test of the route');
+      res.redirect(`/profile/${createdMap.user_id}`);
+    })
+    .catch(err => {
+      console.error("Error occurred:", err);
+      res.status(500).json({ error: err.message });
+    });
+});
+
+module.exports = router;
+
+
 
 module.exports = router;
