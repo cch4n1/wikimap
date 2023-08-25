8ced9889bccd4225da6190ef96361d5942584844
Charlie C <zbyabqrc@pm.me>
2023-08-25 23:02:42 +0000

    show map buttons on home page now work separately for logged in and logged out users

--- public/scripts/viewHomepageMaps.js
+++ public/scripts/viewHomepageMaps.js
@@ -5,16 +5,22 @@ $(document).ready(function() {
     method: "GET",
     url: "/api/viewHomepageMaps",
   }).done(function(response) {
+    let currentURL = $(location).attr('href'); //"http://localhost:8080/home" "http://localhost:8080/home/1"
+    let splitURL = currentURL.split('/'); // ['http:', 'localhost:8080', 'home']  ['http', 'localhost:8080', 'home', '1']
+    let lastElement = splitURL[splitURL.length - 1];  // 'home'       '1'
+    let newURL = '/'
+    if(!isNaN(lastElement[0])){
+      newURL += lastElement[0]
+    }
+   
     const maps = response.maps;
-    console.log(maps)
     for (const map of maps) {
-      console.log(map)
       const tableRow = `
         <tr>
           <td>${map.title}</td>
           <td>${map.username}</td>
           <td>
-            <form method="GET" action="/maps/${map.id}">
+            <form method="GET" action="/maps/${map.id}${newURL}">
               <button type="submit" class="btn btn-outline-danger">Show Map</button>
             </form>
           </td>
