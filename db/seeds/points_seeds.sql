18677f9187ac4cbf4afc998c2074f9ee7a50bb51
Charlie C <zbyabqrc@pm.me>
2023-08-26 09:26:48 +0000

    new seed data

--- db/seeds/points_seeds.sql
+++ db/seeds/points_seeds.sql
@@ -1,28 +1,77 @@
+-- User 1 maps
+INSERT INTO points (map_id, title, description, latitude, longitude, image)
+VALUES
+  (1, 'Griffith Observatory', 'Iconic observatory with city views', 34.1184, -118.3004, 'https://images.pexels.com/photos/9472595/pexels-photo-9472595.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load'),
+  (1, 'Graumans Chinese Theatre', 'Famous theatre with celebrity stars', 34.1014, -118.3410, 'https://images.pexels.com/photos/12464432/pexels-photo-12464432.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+  (1, 'Venice Beach', 'Nice beach', 33.9850, 118.4695, 'https://images.pexels.com/photos/3434562/pexels-photo-3434562.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+  (1, 'Getty Center', 'Art museum with stunning architecture', 34.0776, -118.4740, 'https://images.pexels.com/photos/258846/pexels-photo-258846.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+  (1, 'Santa Monica Pier', 'Historic pier with amusements and ocean views', 34.0095, -118.4969, 'https://images.pexels.com/photos/2531236/pexels-photo-2531236.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'), 
+  (1, 'In-N-Out', 'Animal Style', 34.0295, -118.4800, 'https://images.pexels.com/photos/11483823/pexels-photo-11483823.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'), 
+  (1, 'Hollywood Sign', 'Iconic sign in the Hollywood Hills', 34.1341, -118.3215, 'https://images.pexels.com/photos/5688653/pexels-photo-5688653.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+  (1, 'Dr. Dre House', 'Residence of Dr. Dre in Compton', 33.8949, -118.2201, 'https://upload.wikimedia.org/wikipedia/en/1/19/Dr.DreTheChronic.jpg');
 
--- Insert points for map 1
-INSERT INTO points (map_id, title, description, latitude, longitude, image) VALUES (1, 'Picnic Spot', 'A serene spot for picnics.', 34.0522, -118.2437, 'https://images.pexels.com/photos/977967/pexels-photo-977967.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2');
+INSERT INTO points (map_id, title, description, latitude, longitude, image)
+VALUES
+  (2, 'Robot Restaurant', 'An iconic dinner show experience', 35.659486, 139.700823, 'https://images.pexels.com/photos/5143280/pexels-photo-5143280.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+  (2, 'Bar ABC', 'A cozy bar with live music', 35.688102, 139.761648, 'https://images.pexels.com/photos/6488212/pexels-photo-6488212.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+  (2, 'Karaoke Lounge', 'Sing your heart out here', 35.696728, 139.692388, 'https://images.pexels.com/photos/1601775/pexels-photo-1601775.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+  (2, 'Sushi Restaurant', 'Enjoy fresh sushi', 35.665386, 139.757547, 'https://images.pexels.com/photos/5220092/pexels-photo-5220092.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+  (2, 'Rooftop Bar', 'Great view of the city', 35.699974, 139.771073, 'https://images.pexels.com/photos/12238815/pexels-photo-12238815.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+  (2, 'Dance Club', 'Dance till dawn', 35.675905, 139.732116, 'https://images.pexels.com/photos/2034851/pexels-photo-2034851.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2');
 
-INSERT INTO points (map_id, title, description, latitude, longitude, image) VALUES (1, 'Hiking Trail', 'Scenic hiking trail.', 34.0600, -118.2550, 'https://images.pexels.com/photos/443412/pexels-photo-443412.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2');
+INSERT INTO points (map_id, title, description, latitude, longitude, image)
+VALUES
+  (3, 'The Coffee Collective', 'Specialty coffee in Norrebro', 55.6918, 12.5606, 'https://images.pexels.com/photos/4450323/pexels-photo-4450323.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+  (3, 'Copenhagen Coffee Lab', 'Cozy cafe in Vesterbro', 55.6725, 12.5521, 'https://images.pexels.com/photos/3285137/pexels-photo-3285137.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+  (3, 'Democratic Coffee', 'Coffee and pastries in the city center', 55.6820, 12.5843, 'https://images.pexels.com/photos/1841556/pexels-photo-1841556.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+  (3, 'Original Coffee', 'Coffee with a view in Christianshavn', 55.6740, 12.5902, 'https://images.pexels.com/photos/2467287/pexels-photo-2467287.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+  (3, 'Kafferiet', 'Hidden gem for coffee enthusiasts', 55.6628, 12.5911, 'https://images.pexels.com/photos/416024/pexels-photo-416024.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2');
 
-INSERT INTO points (map_id, title, description, latitude, longitude, image) VALUES (1, 'Wildlife Spotting', 'Great for wildlife enthusiasts.', 34.0510, -118.2495, 'https://images.pexels.com/photos/158986/pexels-photo-158986.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2');
+-- User 2 maps
+INSERT INTO points (map_id, title, description, latitude, longitude, image) 
+VALUES 
+  (4, 'Cove Viewpoint', 'Panoramic view of the cove.', 21.306413, -157.650851, 'https://images.pexels.com/photos/792375/pexels-photo-792375.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'), 
+  (4, 'Beach Access', 'Direct access to the beach.', 21.286066, -157.670631, 'https://images.pexels.com/photos/1049298/pexels-photo-1049298.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),  
+  (4, 'Surfing Spot', 'Ideal for surfers.', 21.262209, -157.697468, 'https://images.pexels.com/photos/1650730/pexels-photo-1650730.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'); 
 
--- Insert points for map 4
-INSERT INTO points (map_id, title, description, latitude, longitude, image) VALUES (4, 'Cove Viewpoint', 'Panoramic view of the cove.', 21.306413, -157.650851, 'https://images.pexels.com/photos/792375/pexels-photo-792375.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2');
+INSERT INTO points (map_id, title, description, latitude, longitude, image)
+VALUES
+  (5, 'Grouse Mountain Trail', 'Challenging hike with stunning views', 49.3697, -123.1027, 'https://images.pexels.com/photos/18002073/pexels-photo-18002073/free-photo-of-landscape-with-a-mountain-peak-and-a-lake.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+  (5, 'Pacific Spirit Regional Park', 'Beautiful forested trails', 49.2533, -123.1937, 'https://images.pexels.com/photos/7276991/pexels-photo-7276991.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+  (5, 'Lynn Canyon Park', 'Suspension bridge and nature trails', 49.3429, -123.0167, 'https://images.pexels.com/photos/3988366/pexels-photo-3988366.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+  (5, 'Cypress Mountain Trail', 'Scenic hiking in the North Shore Mountains', 49.3969, -123.1942, 'https://images.pexels.com/photos/2406454/pexels-photo-2406454.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+  (5, 'Stawamus Chief Trail', 'Rocky cliffs and panoramic views', 49.6794, -123.1392, 'https://images.pexels.com/photos/3881676/pexels-photo-3881676.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2');
 
-INSERT INTO points (map_id, title, description, latitude, longitude, image) VALUES (4, 'Beach Access', 'Direct access to the beach.', 21.286066, -157.670631, 'https://images.pexels.com/photos/1049298/pexels-photo-1049298.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'); 
-
-INSERT INTO points (map_id, title, description, latitude, longitude, image) VALUES (4, 'Surfing Spot', 'Ideal for surfers.', 21.262209, -157.697468, 'https://images.pexels.com/photos/1650730/pexels-photo-1650730.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'); 
-
+INSERT INTO points (map_id, title, description, latitude, longitude, image)
+VALUES
+  (6, 'Ubud Monkey Forest', 'Sacred monkey sanctuary in Ubud', -8.5188, 115.2582, 'https://images.pexels.com/photos/14167156/pexels-photo-14167156.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+  (6, 'Tanah Lot Temple', 'Iconic sea temple on the coast', -8.6217, 115.0856, 'https://images.pexels.com/photos/3225531/pexels-photo-3225531.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+  (6, 'Tegallalang Rice Terraces', 'Stunning rice terraces in Ubud', -8.4317, 115.2745, 'https://images.pexels.com/photos/17795512/pexels-photo-17795512.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+  (6, 'Uluwatu Temple', 'Clifftop temple with Kecak dance performances', -8.8233, 115.0830, 'https://images.pexels.com/photos/2426565/pexels-photo-2426565.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+  (6, 'Mount Batur', 'Volcano trekking and sunrise views', -8.2430, 115.3756, 'https://images.pexels.com/photos/5547180/pexels-photo-5547180.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2');
 
 -- Insert points for map 5
-INSERT INTO points (map_id, title, description, latitude, longitude, image) VALUES (7, 'Tower of London', 'Significant historical site.', 51.5081, -0.0759, 'https://images.pexels.com/photos/5820481/pexels-photo-5820481.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2');
+INSERT INTO points (map_id, title, description, latitude, longitude, image) 
+VALUES 
+  (7, 'Tower of London', 'Significant historical site.', 51.5081, -0.0759, 'https://images.pexels.com/photos/5820481/pexels-photo-5820481.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'), 
+  (7, 'British Museum', 'Visit the local museum.', 51.5194, -0.1270, 'https://images.pexels.com/photos/14845978/pexels-photo-14845978.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'), 
+  (7, 'Tate Modern Art Gallery', 'Explore contemporary art.', 51.5076, -0.0994, 'https://images.pexels.com/photos/11314921/pexels-photo-11314921.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'), 
+  (7, 'Buckingham Palace', 'Residence of the Royal Monarch of UK.', 51.501476, -0.140634, 'https://images.pexels.com/photos/12566687/pexels-photo-12566687.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'), 
+  (7, 'Picadilly Circus', 'Busy meeting place and a tourist attraction.', 51.510067, -0.133869, 'https://images.pexels.com/photos/34639/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'), 
+  (7, 'Palace of Westminister', 'Meeting place for the House of Commons.', 51.4929, -0.1215, 'https://images.pexels.com/photos/13433495/pexels-photo-13433495.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2');
 
-INSERT INTO points (map_id, title, description, latitude, longitude, image) VALUES (7, 'British Museum', 'Visit the local museum.', 51.5194, -0.1270, 'https://images.pexels.com/photos/14845978/pexels-photo-14845978.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2');
+INSERT INTO points (map_id, title, description, latitude, longitude, image)
+VALUES
+  (8, 'Eiffel Tower', 'Iconic Paris landmark', 48.8584, 2.2945, 'https://images.pexels.com/photos/3214982/pexels-photo-3214982.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+  (8, 'Arc de Triomphe', 'Historic arch on the Champs-Elysees', 48.8738, 2.2950, 'https://images.pexels.com/photos/2344/cars-france-landmark-lights.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+  (8, 'Louvre Museum', 'World-famous art museum', 48.8606, 2.3376, 'https://images.pexels.com/photos/2363/france-landmark-lights-night.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+  (8, 'Notre-Dame Cathedral', 'Gothic masterpiece on the Seine', 48.8566, 2.3522, 'https://images.pexels.com/photos/705766/pexels-photo-705766.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+  (8, 'Montmartre', 'Hilltop village with artistic history', 48.8867, 2.3431, 'https://images.pexels.com/photos/2574631/pexels-photo-2574631.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2');
 
-INSERT INTO points (map_id, title, description, latitude, longitude, image) VALUES (7, 'Tate Modern Art Gallery', 'Explore contemporary art.', 51.5076, -0.0994, 'https://images.pexels.com/photos/11314921/pexels-photo-11314921.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2');
-
-INSERT INTO points (map_id, title, description, latitude, longitude, image) VALUES (7, 'Buckingham Palace', 'Residence of the Royal Monarch of UK.', 51.501476, -0.140634, 'https://images.pexels.com/photos/12566687/pexels-photo-12566687.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2');
-
-INSERT INTO points (map_id, title, description, latitude, longitude, image) VALUES (7, 'Picadilly Circus', 'Busy meeting place and a tourist attraction.', 51.510067, -0.133869, 'https://images.pexels.com/photos/34639/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2');
-
-INSERT INTO points (map_id, title, description, latitude, longitude, image) VALUES (7, 'Palace of Westminister', 'Meeting place for the House of Commons.', 51.4929, -0.1215, 'https://images.pexels.com/photos/13433495/pexels-photo-13433495.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2');
+INSERT INTO points (map_id, title, description, latitude, longitude, image)
+VALUES
+  (9, 'Sagrada Família', 'Gaudís masterpiece basilica', 41.4036, 2.1744, 'https://images.pexels.com/photos/1338393/pexels-photo-1338393.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+  (9, 'Park Güell', 'Artistic park with colorful mosaics', 41.4145, 2.1527, 'https://images.pexels.com/photos/14051747/pexels-photo-14051747.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+  (9, 'La Rambla', 'Famous tree-lined street in the city center', 41.3797, 2.1731, 'https://images.pexels.com/photos/2379980/pexels-photo-2379980.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+  (9, 'Barri Gòtic', 'Historic Gothic Quarter', 41.3834, 2.1764, 'https://images.pexels.com/photos/4383777/pexels-photo-4383777.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+  (9, 'Magic Fountain of Montjuïc', 'Colorful fountain with nightly shows', 41.3711, 2.1514, 'https://images.pexels.com/photos/6642509/pexels-photo-6642509.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2');
